369d55de5b0bbd7f03de2fb2dbae0167b493708a
cherylchao6 <nmpyt21@gmail.com>
2024-05-25 08:20:52 -0400

    limit cor origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://iwantthisjobsobad.org',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   await app.listen(8080);
 }
 bootstrap();
